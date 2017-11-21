b6a7bc468cda7f579762167970b9301d871b1cf9
Ravi Kukreja <ravi_kukreja@mastercard.com>
2017-11-21 12:05:21 +0530

    Update

--- auto/src/test/java/AutoITProj/auto/FirstTest.java
+++ auto/src/test/java/AutoITProj/auto/FirstTest.java
@@ -8,7 +8,12 @@ import junit.framework.TestCase;
 public class FirstTest extends TestCase {
 
 	public void testfirstTest() throws InterruptedException, IOException {
-		String commandToExecute = "first.exe";
+		String command = "cmd /c";
+		System.out.println(command);
+		String fileName = " first.exe";
+		String commandToExecute = command+fileName;
+		System.out.println(commandToExecute);
+		
 		Runtime.getRuntime().exec(commandToExecute).waitFor(2, TimeUnit.SECONDS);
 	}
 }
