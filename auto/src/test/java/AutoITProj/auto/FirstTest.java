f6700d6e81bf9ef5e561d2209a84db871eaf5e06
Ravi Kukreja <ravi_kukreja@mastercard.com>
2017-11-21 11:17:51 +0530

    Commit 2

--- auto/src/test/java/AutoITProj/auto/FirstTest.java
+++ auto/src/test/java/AutoITProj/auto/FirstTest.java
@@ -1,14 +1,14 @@
+package AutoITProj.auto;
+
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import junit.framework.TestCase;
 
-public class Test {
-	
-	public static void main(String[] args) throws InterruptedException, IOException {
+public class FirstTest extends TestCase {
+
+	public void testfirstTest() throws InterruptedException, IOException {
 		String commandToExecute = "first.exe";
 		Runtime.getRuntime().exec(commandToExecute).waitFor(2, TimeUnit.SECONDS);
-
-		
 	}
-
 }
